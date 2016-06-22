42abc28b9c4cf6e20cd8f77b11fde203c0403d11
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-06-22 21:53:42 +0800

    Moaning has began

---
+++ action.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function update(ev) {
+  console.log(ev);
+
+  var hratio = ev.screenX / $(window).height();
+  var wratio = ev.screenY / $(window).width();
+
+  $(".bg").css("transform", "translate(" + (hratio - 0.5) * 20 + "px, " + (wratio -0.5) * 20 + "px)");
+  $(".brand").css("transform", "translate(" + (hratio - 0.5) * 40 + "px, " + (wratio -0.5) * 40 + "px)");
+}
+
+$(document).ready(function() {
+  $(window).mousemove(update);
+  $(window).resize(update);
+});
